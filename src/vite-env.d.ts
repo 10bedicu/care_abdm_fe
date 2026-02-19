c92329132ca63a1dd46a11c64370c869b8a05eed
Khavin Shankar <khavinshankar@gmail.com>
2026-02-19 13:38:34 +0530

    added types for window meta config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,8 +4,30 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface ImportMetaEnv {}
 
+interface CareAbdmFePluginConfig {
+  faceAuthUrl?: string;
+  enforceAbhaNumberLinking?: boolean;
+  scanAndShareUrl?: string;
+}
+
+interface CarePluginRuntimeMeta {
+  care_abdm_fe?: {
+    config?: CareAbdmFePluginConfig;
+  };
+}
+
+interface CarePluginRuntime {
+  meta?: CarePluginRuntimeMeta;
+}
+
 declare global {
   const __CORE_ENV__: {
     readonly apiUrl: string;
   };
+
+  interface Window {
+    __CARE_PLUGIN_RUNTIME__?: CarePluginRuntime;
+  }
 }
+
+export {};
